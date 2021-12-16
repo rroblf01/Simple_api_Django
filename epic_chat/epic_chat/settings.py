69852c1fcd2d8f3a42503b56a7bd72a25d6817c7
rroblf01 <rroblf01@estudiantes.unileon.es>
2021-12-16 16:01:38 +0100

    Update settings.py

--- epic_chat/epic_chat/settings.py
+++ epic_chat/epic_chat/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oiz%^m)8j@$0-&a5ij26czg-r(1=1+^i*yyh&#jwjkf2i1o8(p'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
